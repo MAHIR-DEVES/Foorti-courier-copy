c2a90e1a1db64c6c869d7380494759092b9dc56c
Md Meherab Hossain Munna <mdmeherab657@gamil.com>
2026-01-11 19:18:13 +0600

    responcive issue fix

--- app/components/dashboard/Card.jsx
+++ app/components/dashboard/Card.jsx
@@ -21,15 +21,34 @@ const performanceData = [
 const Card = () => {
   return (
     <div
-      className="grid grid-cols-4 gap-3 pb-2 mt-5
-    "
+      className="
+    grid 
+    grid-cols-1 
+    sm:grid-cols-2 
+    md:grid-cols-3 
+    lg:grid-cols-4 
+    gap-3 
+    pb-2 
+    mt-5
+  "
     >
       {performanceData.map((item, index) => (
         <div
           key={index}
-          className="md:flex justify-between gap-3 px-2 py-3 md:py-4 bg-primary border border-gray rounded-md"
+          className="
+        flex 
+        justify-between 
+        gap-3 
+        px-2 
+        py-3 
+        md:py-4 
+        bg-primary 
+        border 
+        border-gray 
+        rounded-md
+      "
         >
-          <div className="md:block">
+          <div>
             <h2 className="text-[20px] text-primary font-semibold whitespace-nowrap">
               {item.title} 0
             </h2>
